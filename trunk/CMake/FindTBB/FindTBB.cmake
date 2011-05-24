58275c29a957879204c019642ffcbabbabdaa04a
hannes.hofmann@informatik.uni-erlangen.de <hannes.hofmann@informatik.uni-erlangen.de@cdd17612-e7c0-11dd-b00f-9fabb4b2ff13>
2011-05-24 13:06:49 +0000

    Added define of TBB_INTERFACE_VERSION (thx to facebook). Minor changes (whitespace, error messages)

--- trunk/CMake/FindTBB/FindTBB.cmake
+++ trunk/CMake/FindTBB/FindTBB.cmake
@@ -1,17 +1,19 @@
 # Locate Intel Threading Building Blocks include paths and libraries
-# TBB can be found at http://www.threadingbuildingblocks.org/ 
-# Written by Hannes Hofmann, hannes.hofmann _at_ informatik.uni-erlangen.de
-# Adapted by Gino van den Bergen gino _at_ dtecta.com
+# FindTBB.cmake can be found at https://code.google.com/p/findtbb/
+# Written by Hannes Hofmann <hannes.hofmann _at_ informatik.uni-erlangen.de>
+# Improvements by Gino van den Bergen <gino _at_ dtecta.com>, Florian Uhlig <F.Uhlig _at_ gsi.de>
 
 # GvdB: This module uses the environment variable TBB_ARCH_PLATFORM which defines architecture and compiler.
 #   e.g. "ia32/vc8" or "em64t/cc4.1.0_libc2.4_kernel2.6.16.21"
 #   TBB_ARCH_PLATFORM is set by the build script tbbvars[.bat|.sh|.csh], which can be found
 #   in the TBB installation directory (TBB_INSTALL_DIR).
 #
+# GvdB: Mac OS X distribution places libraries directly in lib directory.
+#
 # For backwards compatibility, you may explicitely set the CMake variables TBB_ARCHITECTURE and TBB_COMPILER.
-# TBB_ARCHITECTURE     [ ia32 | em64t | itanium ]
+# TBB_ARCHITECTURE [ ia32 | em64t | itanium ]
 #   which architecture to use
-# TBB_COMPILER         e.g. vc9 or cc3.2.3_libc2.3.2_kernel2.4.21 or cc4.0.1_os10.4.9
+# TBB_COMPILER e.g. vc9 or cc3.2.3_libc2.3.2_kernel2.4.21 or cc4.0.1_os10.4.9
 #   which compiler to use (detected automatically on Windows)
 
 # This module respects
@@ -24,11 +26,12 @@
 # TBB_LIBRARIES, the libraries to link against to use TBB.
 # TBB_DEBUG_LIBRARIES, the libraries to link against to use TBB with debug symbols.
 # TBB_FOUND, If false, don't try to use TBB.
+# TBB_INTERFACE_VERSION, as defined in tbb/tbb_stddef.h
 
 
 if (WIN32)
-    # has em64t/vc8   em64t/vc9
-    # has ia32/vc7.1  ia32/vc8   ia32/vc9
+    # has em64t/vc8 em64t/vc9
+    # has ia32/vc7.1 ia32/vc8 ia32/vc9
     set(_TBB_DEFAULT_INSTALL_DIR "C:/Program Files/Intel/TBB" "C:/Program Files (x86)/Intel/TBB")
     set(_TBB_LIB_NAME "tbb")
     set(_TBB_LIB_MALLOC_NAME "${_TBB_LIB_NAME}malloc")
@@ -68,7 +71,7 @@ if (UNIX)
         set(_TBB_LIB_MALLOC_NAME "${_TBB_LIB_NAME}malloc")
         set(_TBB_LIB_DEBUG_NAME "${_TBB_LIB_NAME}_debug")
         set(_TBB_LIB_MALLOC_DEBUG_NAME "${_TBB_LIB_MALLOC_NAME}_debug")
-        # has em64t/cc3.2.3_libc2.3.2_kernel2.4.21  em64t/cc3.3.3_libc2.3.3_kernel2.6.5  em64t/cc3.4.3_libc2.3.4_kernel2.6.9  em64t/cc4.1.0_libc2.4_kernel2.6.16.21
+        # has em64t/cc3.2.3_libc2.3.2_kernel2.4.21 em64t/cc3.3.3_libc2.3.3_kernel2.6.5 em64t/cc3.4.3_libc2.3.4_kernel2.6.9 em64t/cc4.1.0_libc2.4_kernel2.6.16.21
         # has ia32/*
         # has itanium/*
         set(_TBB_COMPILER ${TBB_COMPILER})
@@ -117,7 +120,7 @@ if (NOT _TBB_INSTALL_DIR)
 endif (NOT _TBB_INSTALL_DIR)
 # sanity check
 if (NOT _TBB_INSTALL_DIR)
-    message ("ERROR: TBB_INSTALL_DIR not found. ${_TBB_INSTALL_DIR}")
+    message ("ERROR: Unable to find Intel TBB install directory. ${_TBB_INSTALL_DIR}")
 else (NOT _TBB_INSTALL_DIR)
 # finally: set the cached CMake variable TBB_INSTALL_DIR
 if (NOT TBB_INSTALL_DIR)
@@ -126,7 +129,7 @@ if (NOT TBB_INSTALL_DIR)
 endif (NOT TBB_INSTALL_DIR)
 
 
-#-- A macro to rewrite the paths of the library. This is necessary, because 
+#-- A macro to rewrite the paths of the library. This is necessary, because
 #   find_library() always found the em64t/vc9 version of the TBB libs
 macro(TBB_CORRECT_LIB_DIR var_name)
 #    if (NOT "${_TBB_ARCHITECTURE}" STREQUAL "em64t")
@@ -144,7 +147,6 @@ set (TBB_INC_SEARCH_DIR ${_TBB_INSTALL_DIR}/include)
 find_path(TBB_INCLUDE_DIR
     tbb/task_scheduler_init.h
     PATHS ${TBB_INC_SEARCH_DIR}
-    NO_DEFAULT_PATH
 )
 mark_as_advanced(TBB_INCLUDE_DIR)
 
@@ -165,8 +167,7 @@ endif (NOT $ENV{TBB_ARCH_PLATFORM} STREQUAL "")
 # GvdB: Mac OS X distribution places libraries directly in lib directory.
 list(APPEND _TBB_LIBRARY_DIR ${_TBB_INSTALL_DIR}/lib)
 
-
-find_library(TBB_LIBRARY        ${_TBB_LIB_NAME}        ${_TBB_LIBRARY_DIR} NO_DEFAULT_PATH)
+find_library(TBB_LIBRARY ${_TBB_LIB_NAME} ${_TBB_LIBRARY_DIR} NO_DEFAULT_PATH)
 find_library(TBB_MALLOC_LIBRARY ${_TBB_LIB_MALLOC_NAME} ${_TBB_LIBRARY_DIR} NO_DEFAULT_PATH)
 
 #Extract path from TBB_LIBRARY name
@@ -206,3 +207,10 @@ if (NOT TBB_FOUND)
 endif (NOT TBB_FOUND)
 
 endif (NOT _TBB_INSTALL_DIR)
+
+if (TBB_FOUND)
+	set(TBB_INTERFACE_VERSION 0)
+	FILE(READ "${TBB_INCLUDE_DIRS}/tbb/tbb_stddef.h" _TBB_VERSION_CONTENTS)
+	STRING(REGEX REPLACE ".*#define TBB_INTERFACE_VERSION ([0-9]+).*" "\\1" TBB_INTERFACE_VERSION "${_TBB_VERSION_CONTENTS}")
+	set(TBB_INTERFACE_VERSION "${TBB_INTERFACE_VERSION}")
+endif (TBB_FOUND)
